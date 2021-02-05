f693e7ef363f8913ab67338c159b8ea787f65d2c
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-05 23:12:12 +0900

    added hanburger-menu in sp header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,15 @@ class Header extends React.Component{
                 <div className="header__wrapper">
                     <div className="header__inner">
                         <img src={TwitterIcon} alt=""/>
+
                         <nav className="header__nav">
                             <div>menu1</div>
                             <div>menu2</div>
                             <div>menu3</div>
+                        </nav>
+
+                        <nav className="header__hamburger">
+                            <div>三</div>
                         </nav>
                     </div>
                 </div>
