37726b6cd966bfa6d251ab160a7f14cd4b4b7224
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-06 23:13:16 +0900

    refactored min-height of timeine

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,73 +1,46 @@
 import React from 'react'
 
 import '../scss/Main.scss'
+import '../scss/parts/_TwitterCard.scss'
 
 import Person from '../img/person-icon.png'
 
 
 class Main extends React.Component{
 
-    tweets = [
-        {
-            "id":"1",
-            "content":"初めてのツイート",
-            "created_at":"2020-01-31",
-            "user":{
-                "id":"1",
-                "name":"first-user",
-                "mail":"abc@example.com",
-            },
-        },
-        {
-            "id":"2",
-            "content":"2回目のツイート",
-            "created_at":"2020-01-31",
-            "user":{
-                "id":"1",
-                "name":"first-user",
-                "mail":"abc@example.com",
-            },
-        },
-        {
-            "id":"3",
-            "content":"3回目のツイート",
-            "created_at":"2020-01-31",
-            "user":{
-                "id":"1",
-                "name":"first-user",
-                "mail":"abc@example.com",
-            },
-        },
-        {
-            "id":"4",
-            "content":"4回目のツイート",
-            "created_at":"2020-01-31",
-            "user":{
-                "id":"1",
-                "name":"first-user",
-                "mail":"abc@example.com",
-            },
-        },
-        {
-            "id":"5",
-            "content":"5回目のツイート",
-            "created_at":"2020-01-31",
-            "user":{
-                "id":"1",
-                "name":"first-user",
-                "mail":"abc@example.com",
-            },
-        },
-    ]
+    makeTweets(){
+        let tweets = []
+        for(let i =1; i<50 ; i++){
+            tweets.push({
+                "id":i,
+                "content":`${i}回目のツイート`,
+                "created_at":"2020-01-31",
+                "user":{
+                    "id":"1",
+                    "name":"first-user",
+                    "mail":"abc@example.com",
+                },
+            })
+        }
+        return tweets
+    }
 
     renderList(){
-        return this.tweets.map(tweet => {
+        return this.makeTweets().map(tweet => {
             return (
-                <div className="item" key={tweet.id}>
-                    <img src={Person} alt=""/>
-                    <span>{tweet.user.name}</span>
-                    <span>{tweet.created_at}</span>
-                    <span>{tweet.content}</span>
+                <div className="twitter-card" key={tweet.id}>
+                    <div className="twitter-card__left">
+                        <img src={Person} alt=""/>
+                    </div>
+                    <div className="twitter-card__main">
+                        <div>
+                            <span className="username" >{tweet.user.name}</span>
+                            <span className="date">{tweet.created_at}</span>
+                        </div>
+                        <div>
+                            <span className="tweet">{tweet.content}</span>
+                        </div>
+                    </div>
                 </div>
             )
         })
