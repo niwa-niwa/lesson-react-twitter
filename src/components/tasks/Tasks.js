122ff230440120ff8a050da26f1609f92941f8f0
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-09 23:15:24 +0900

    created new app that called Tasks with mock-server

---
+++ src/components/tasks/Tasks.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const Tasks = () => {
+
+
+    // TODO: post a task
+
+    // TODO: post status done to the task
+
+    // TODO: post status star to the task
+
+    // TODO: fetch tasks
+
+    // TODO: show a dialog to input a task
+
+    // TODO: create a task-card
+
+    // TODO: rendering tasks in a list
+
+    return (
+        <div>Task page</div>
+    )
+}
+
+export default Tasks
