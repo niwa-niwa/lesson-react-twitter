70ab259d33ea46129fe130b66085451842ce6c32
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-11 20:31:15 +0900

    implemented getTasks with axios

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,39 +1,20 @@
-import React,{ useState, useEffect } from 'react'
+import React from 'react'
 
 import TaskCard from './TaskCard'
+import useTasks from './useTasks'
 
 import './TaskCard.scss'
 
 
 const Tasks = () => {
 
-    const [tasks, setTasks] = useState([])
+    const [tasks] = useTasks([])
 
-    useEffect(() => {
-        setTasks([
-            ...[
-                {
-                    "id":"aaaa1111",
-                    "title":"task 1",
-                    "done":false,
-                    "star":false
-                },
-                {
-                    "id":"bbb1111",
-                    "title":"task 2",
-                    "done":false,
-                    "star":false
-                }
-            ]
-        ])
-    }, [])
 
     // TODO: post a task
 
     // TODO: show a dialog to input a task
     
-    // TODO: fetch tasks
-
     // TODO: rendering tasks in a list
     const renderingList = () => {
         return (
---
+++ src/components/tasks/useTasks.js
@@ -0,0 +1,25 @@
+import { useState, useEffect} from 'react'
+import tasksApi from '../../apis/tasks'
+
+
+// fetch tasks
+const useTasks = () => {
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        getTasks()
+    },[])
+
+    const getTasks = async () => {
+        try{
+            const {data} = await tasksApi.get('tasks')
+            setTasks(data)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    return[tasks, setTasks]
+}
+export default useTasks
