af3b66a5fdd1c089a6228ec7e19c7f10a3eb8432
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-25 23:46:34 +0900

    made test-code to test display element after rendering

--- src/components/twitter/Main.js
+++ src/components/twitter/Main.js
@@ -7,23 +7,26 @@ import "./Main.scss"
 
 import Person from "../../img/person-icon.png"
 
+
 const Main = () => {
+
   const { tweets } = useSelector((state) => state.tweetsReducer)
 
+
   const renderList = () => {
-    return tweets.map((tweet) => {
+    return tweets.map((tweet, index) => {
       return (
-        <div className="twitter-card" key={tweet.id}>
+        <div className="twitter-card" key={tweet.id} data-testid={`twitter-card-${index}`}>
           <div className="twitter-card__left">
-            <img src={Person} alt="" />
+            <img src={Person} alt="" data-testid={`card-img-${index}`}/>
           </div>
           <div className="twitter-card__main">
             <div>
-              <span className="username">{tweet.user.name}</span>
-              <span className="date">{tweet.created_at}</span>
+              <span className="username" data-testid={`card-username-${index}`} >{tweet.user.name}</span>
+              <span className="date" data-testid={`card-created_at-${index}`} >{tweet.created_at}</span>
             </div>
             <div>
-              <span className="tweet">{tweet.content}</span>
+              <span className="tweet" data-testid={`card-content-${index}`} >{tweet.content}</span>
             </div>
           </div>
         </div>
--- src/components/twitter/tests/Main.test.js
+++ src/components/twitter/tests/Main.test.js
@@ -10,27 +10,50 @@ import tweetSlice from "../../../stores/tweetsSlice"
 import jsonSlice from "../../../stores/JsonSlice"
 
 import Main from '../Main'
-// import {store} from '../../../stores'
 
 describe("Tweet-Main-component test cases", () => {
 
   let store;
   beforeEach(() => {
+
+    // ready to redux store
     store = configureStore({
       reducer: {
         tweetsReducer: tweetSlice.reducer,
         jsonReducer: jsonSlice.reducer
       },
     });
-  });
 
-it("1 :render all the elements correctly", async ()=> {
+    // ready to routing
     render(
       <Provider store={store}>
         <Main />
       </Provider>
     )
+  });
 
-    screen.debug()
+
+it("1 :render all the elements correctly",  ()=> {
+    
+    // display source that is complied js
+    // screen.debug()
+
+    // Twitter-form test
+    expect(screen.getByTestId("tweet-form")).toBeTruthy()
+    expect(screen.getByTestId("tweet-form-textarea")).toBeTruthy()
+    expect(screen.getByTestId("tweet-form-submit")).toBeTruthy()
+    
+    // Twitter-card test
+    expect(screen.getByTestId("twitter-card-0")).toBeTruthy()
+    expect(screen.getByTestId("card-img-0")).toBeTruthy()
+    expect(screen.getByTestId("card-username-0")).toBeTruthy()
+    expect(screen.getByTestId("card-created_at-0")).toBeTruthy()
+    expect(screen.getByTestId("card-content-0")).toBeTruthy()
+    
+    // Twitter-card test to have text
+    expect(screen.getByText("first-user")).toBeTruthy()
+    expect(screen.getByText("2020-01-31")).toBeTruthy()
+    expect(screen.getByText("1回目のツイート")).toBeTruthy()
   })
+
 })
