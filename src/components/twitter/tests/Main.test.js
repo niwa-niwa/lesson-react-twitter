c506311f743ac11bffa4b234df15a51b28f4f7ae
niwa-niwa <pass_mail39-sns@yahoo.co.jp>
2021-02-26 21:56:20 +0900

    added test-code of use action input and click

--- src/components/twitter/tests/Main.test.js
+++ src/components/twitter/tests/Main.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, cleanup } from '@testing-library/react'
+import { render, screen, cleanup} from '@testing-library/react'
 import userEvent from "@testing-library/user-event"
 import { rest } from "msw"
 import { setupServer } from "msw/node"
@@ -33,6 +33,9 @@ describe("Tweet-Main-component test cases", () => {
   });
 
 
+  afterEach(cleanup)
+
+
 it("1 :render all the elements correctly",  ()=> {
     
     // display source that is complied js
@@ -54,6 +57,37 @@ it("1 :render all the elements correctly",  ()=> {
     expect(screen.getByText("first-user")).toBeTruthy()
     expect(screen.getByText("2020-01-31")).toBeTruthy()
     expect(screen.getByText("1回目のツイート")).toBeTruthy()
+  })
+
+  it("2 :Twitter Form test", () => {
+    // screen.debug()
+    const tweet = "test tweet to me"
+
+    // not display new tweet data
+    expect(screen.queryByTestId("twitter-card-1")).toBeNull()
+    expect(screen.queryByTestId("card-img-1")).toBeNull()
+    expect(screen.queryByTestId("card-username-1")).toBeNull()
+    expect(screen.queryByTestId("card-created_at-1")).toBeNull()
+    expect(screen.queryByTestId("card-content-1")).toBeNull()
+    
+    // send new tweet 
+    expect(screen.getByPlaceholderText("write your feel")).toBeTruthy()
+    const inputValue = screen.getByTestId("tweet-form-textarea")
+    userEvent.type(inputValue, tweet)
+    userEvent.click(screen.getByTestId("tweet-form-submit"))
+
+    // appear new element
+    expect(screen.getByTestId("twitter-card-1")).toBeTruthy()
+    expect(screen.getByTestId("card-img-1")).toBeTruthy()
+    expect(screen.getByTestId("card-username-1")).toBeTruthy()
+    expect(screen.getByTestId("card-created_at-1")).toBeTruthy()
+    expect(screen.getByTestId("card-content-1")).toBeTruthy()
+
+    // display new tweet-data
+    expect(screen.getByTestId("card-username-1").textContent).toBe("first-user")
+    expect(screen.getByTestId("card-created_at-1").textContent).toBe("2020-01-31")
+    expect(screen.getByTestId("card-content-1").textContent).toBe(tweet)
+
   })
 
 })
